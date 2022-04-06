af42b9cfdfc1a07920266306f3e626a9c22b915b
Theophileaseh <theophilenwachan@gmail.com>
2022-04-06 18:37:45 +0100

    Update webpack

--- public/339bcf0c2883a30467ff.js
+++ public/339bcf0c2883a30467ff.js
@@ -1,4 +1,3 @@
 import _ from 'lodash';
 import './scores.js';
-
 import './style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
 
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
