71e5d81f96b34d930af16ef116a506749bce577d
Theophileaseh <theophilenwachan@gmail.com>
2022-04-06 21:13:14 +0100

    Add API and dynamic function files

---
+++ src/modules/dom.js
@@ -0,0 +1,27 @@
+let flag = true;
+export function creatHtml(game) {
+  const list = document.getElementById('scoreList');
+  list.innerHTML = '';
+  game.forEach((item) => {
+    const listelem = document.createElement('li');
+    listelem.className = 'single-score';
+    listelem.innerText = `${item.user}: ${item.score}`;
+    if (flag === false) {
+      listelem.style.background = 'rgb(231 231 231)';
+    }
+    list.appendChild(listelem);
+    flag = !flag;
+  });
+}
+
+export function creatSubhtml(userName, userScore) {
+  const list = document.getElementById('scoreList');
+  const listelem = document.createElement('li');
+  listelem.className = 'single-score';
+  listelem.innerText = `${userName}: ${userScore}`;
+  if (flag === false) {
+    listelem.style.background = 'rgb(231 231 231)';
+  }
+  list.appendChild(listelem);
+  flag = !flag;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
 
-  entry: './src/index.js',
+  entry:['babel-regenerator-runtime', './src/index.js'],
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Leaderboard',
